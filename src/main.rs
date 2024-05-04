29c97618c432f0c076d68227cd2a90d15c557a39
İlyas Akın <ashnwor@gmail.com>
2024-05-04 14:25:12 +0300

    Move everything from main to Tokenizer()

--- src/main.rs
+++ src/main.rs
@@ -25,64 +25,79 @@ impl Token {
     }
 }
 
+struct Tokenizer {}
+
+impl Tokenizer {
+    fn new() -> Tokenizer {
+        Tokenizer {}
+    }
+
+    fn tokenize(&self, code: &str) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+
+        for (index, line) in code.lines().enumerate() {
+            println!("{}", line);
+
+            // calculate line number
+            let line_number: u32 = (index as u32) + 1;
+
+            // Split line with whitespace
+            let words: Vec<&str> = line.split_whitespace().collect();
+
+            for word in words {
+                let column_number: u32 = line.find(word).unwrap() as u32;
+
+                // Check if word is a keyword
+                if word == "var" || word == "let" || word == "const" {
+                    let token: Token = Token::new(
+                        EToken::Keyword(String::from(word)),
+                        line_number,
+                        column_number,
+                    );
+                    tokens.push(token);
+                }
+
+                // Check if word is an identifier
+                if word == "x" {
+                    let token: Token = Token::new(
+                        EToken::Identifier(String::from(word)),
+                        line_number,
+                        column_number,
+                    );
+                    tokens.push(token);
+                }
+
+                // Check if word is an operator
+                if word == "=" || word == "+" || word == "-" || word == "*" || word == "/" {
+                    let token: Token = Token::new(
+                        EToken::Operator(String::from(word)),
+                        line_number,
+                        column_number,
+                    );
+                    tokens.push(token);
+                }
+
+                // TODO: This is a simple check, we need to implement a more complex check
+                if word == "10" {
+                    let token = Token::new(
+                        EToken::Literal(String::from(word)),
+                        line_number,
+                        column_number,
+                    );
+                    tokens.push(token);
+                }
+            }
+        }
+
+        return tokens;
+    }
+}
+
 fn main() {
     let contents = std::fs::read_to_string("./src/samples/variable_declaration.js")
         .expect("Something went wrong reading the file");
 
-    let tokens: Vec<&Token> = vec![];
+    let tokens: Vec<Token> = Tokenizer::new().tokenize(&contents);
 
-    // line with index
-    for (index, line) in contents.lines().enumerate() {
-        println!("{}", line);
-
-        // calculate line number
-        let line_number: u32 = (index as u32) + 1;
-
-        // Split line with whitespace
-        let words: Vec<&str> = line.split_whitespace().collect();
-
-        for word in words {
-            let column_number: u32 = line.find(word).unwrap() as u32;
-
-            // Check if word is a keyword
-            if word == "var" || word == "let" || word == "const" {
-                let token = Token::new(
-                    EToken::Keyword(String::from(word)),
-                    line_number,
-                    column_number,
-                );
-                println!("{:?}", token);
-            }
-
-            // Check if word is an identifier
-            if word == "x" {
-                let token = Token::new(
-                    EToken::Identifier(String::from(word)),
-                    line_number,
-                    column_number,
-                );
-                println!("{:?}", token);
-            }
-
-            // Check if word is an operator
-            if word == "=" || word == "+" || word == "-" || word == "*" || word == "/" {
-                let token = Token::new(
-                    EToken::Operator(String::from(word)),
-                    line_number,
-                    column_number,
-                );
-                println!("{:?}", token);
-            }
-
-            // TODO: This is a simple check, we need to implement a more complex check
-            if word == "10" {
-                let token = Token::new(
-                    EToken::Literal(String::from(word)),
-                    line_number,
-                    column_number,
-                );
-                println!("{:?}", token);
-            }
-        }
-    }
+    println!("{:?}", tokens);
 }
